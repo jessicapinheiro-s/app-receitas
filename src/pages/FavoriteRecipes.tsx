d288b5666b700347377f0a9f495ffabf9d264037
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2025-01-28 07:07:56 -0300

    Contruindo o componente para favorite recipes.

--- src/pages/FavoriteRecipes.tsx
+++ src/pages/FavoriteRecipes.tsx
@@ -1,37 +1,65 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Header from "../components/Header";
 import { getRecipesById } from "../functions API/recipes";
 import { RecipesById } from "../types/api-types";
+import CardRecipes from "../components/Card-Recipe";
 
 
 export default function FavoriteRecipesPage() {
     const [favoriteRecipes, setFavoritesRecipes] = useState<RecipesById[]>();
 
-    async function porra() {
+    /*async function getRecipesByLocalStorage() {
         const itemsFromLocal = JSON.parse(localStorage.getItem('ItemId') || '[]');
-
         if (itemsFromLocal.length > 0) {
-            const favItensInformation = itemsFromLocal?.map(async (item: string) => {
-                try {
-                    return await getRecipesById(parseInt(item));
-                } catch (error) {
-                    return [];
-                }
-            });
+            const favItensInformation = await Promise.all(
+                itemsFromLocal.map(async (id: number) => {
+                    const response = await getRecipesById(id);
+                    return response;
+                })
+            )
             setFavoritesRecipes(favItensInformation);
         }
+    }*/
 
-    }
+    const getRecipesByLocalStorage = useCallback(async () => {
+        const itemsFromLocal = JSON.parse(localStorage.getItem('ItemId') || '[]');
+        if (itemsFromLocal.length > 0) {
+            const favItensInformation = await Promise.all(
+                itemsFromLocal.map(async (id: number) => {
+                    const response = await getRecipesById(id);
+                    return response;
+                })
+            );
 
-    console.log(favoriteRecipes)
+            setFavoritesRecipes((prev) => {
+                if(JSON.stringify(prev) !== JSON.stringify(favItensInformation)) {
+                    return favItensInformation;
+                }
+                return prev;
+            });
+        }
+    }, [])
+
+
+    useEffect(() => {
+        getRecipesByLocalStorage();
+    }, [getRecipesByLocalStorage]);
+
+    
     return (
         <div className="w-full h-full gap-8 flex flex-col">
             <Header />
+            <h1>Favorite Recipes</h1>
             <div className='w-full px-44 flex flex-row items-center justify-start flex-wrap gap-8'>
                 {
-
+                    favoriteRecipes?.map(item => (
+                        <CardRecipes item={{
+                            title: item.title,
+                            image:  item.image,
+                            id:     item.id
+                        }} index={item.id} />
+                    ))
                 }
-
             </div>
         </div>
     )
